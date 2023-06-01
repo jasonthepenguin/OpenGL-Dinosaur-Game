cb890a893357a3c7850d5ebe579fc6615df13dc3
jasonthepenguin <jasonbotterill@hotmail.com>
2023-06-02 04:07:28 +0800

    Moving raw files

--- Lab_4/Lua/Terrain.lua
+++ Lab_4/Lua/Terrain.lua
@@ -6,7 +6,7 @@ terrain = {
     {
         startPos = {x = 0, y = 0, z = 0},
         scale = { x = 3.0, y = 0.1, z = 3.0},
-        rawFilePath = "newtest.raw",
+        rawFilePath = "heightmaps/newtest.raw",
         heightfieldSize = 128,
         usesProceduralTexture = true
     }
